1081bfe82c321eb32bea9f11817bdcc49f576150
Icaro <icarocordeirodecarvalho7@gmail.com>
2026-09-21 12:47:38 -0300

    Permitir conexão local do Freddy

--- android/app/src/main/java/com/icaro/freddyai/MainActivity.java
+++ android/app/src/main/java/com/icaro/freddyai/MainActivity.java
@@ -1,5 +1,19 @@
 package com.icaro.freddyai;
 
+import android.webkit.WebSettings;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        if (getBridge() != null && getBridge().getWebView() != null) {
+            WebSettings settings = getBridge().getWebView().getSettings();
+            settings.setMixedContentMode(
+                WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+            );
+        }
+    }
+}
